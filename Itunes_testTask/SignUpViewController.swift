4c46b99f258def3cc680cc50ef8e2f4cc7917e33
Pavelweb22 <pavelweb2016@yandex.ru>
2023-01-25 21:22:23 +0300

    SignUpViewController

--- Itunes_testTask/SignUpViewController.swift
+++ Itunes_testTask/SignUpViewController.swift
@@ -9,6 +9,19 @@ import UIKit
 
 class SignUpViewController: UIPageViewController {
 
+//    private let scrollView: UIScrollView = {
+//        let scrollView = UIScrollView()
+//        scrollView.translatesAutoresizingMaskIntoConstraints = false
+//        return scrollView
+//    }()
+//
+//    private let backgroundView: UIView = {
+//        let view = UIView()
+//        view.backgroundColor = .white
+//        view.translatesAutoresizingMaskIntoConstraints = false
+//        return view
+//    }()
+    
     private let regLabel: UILabel = {
         let label = UILabel()
         label.text = "Registration"
@@ -16,6 +29,21 @@ class SignUpViewController: UIPageViewController {
         return label
     }()
     
+    private let firstNameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "First Name"
+        textField.borderStyle = .roundedRect
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    private let firstNameValidLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Required field"
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     
     
     override func viewDidLoad() {
@@ -29,6 +57,8 @@ class SignUpViewController: UIPageViewController {
     
     private func setupViews() {
         view.addSubview(regLabel)
+        view.addSubview(firstNameTextField)
+        view.addSubview(firstNameValidLabel)
     }
     
 }
@@ -39,8 +69,16 @@ extension SignUpViewController {
     
     private func setConstraints() {
         NSLayoutConstraint.activate([
-            regLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
-            regLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            regLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
+            regLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            firstNameTextField.topAnchor.constraint(equalTo: regLabel.bottomAnchor, constant: 30),
+            firstNameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12),
+            firstNameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -12),
+            firstNameTextField.heightAnchor.constraint(equalToConstant: 40),
+            
+            firstNameValidLabel.topAnchor.constraint(equalTo: firstNameTextField.bottomAnchor, constant: 10),
+            firstNameValidLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12)
         ])
     }
 }
