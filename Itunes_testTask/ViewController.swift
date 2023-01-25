73778a5b3829e2c64f2d413374b70d5ae230c6d5
Pavelweb22 <pavelweb2016@yandex.ru>
2023-01-25 20:16:43 +0300

    setupProject

--- Itunes_testTask/ViewController.swift
+++ Itunes_testTask/ViewController.swift
@@ -9,11 +9,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
+    private let backgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let loginLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Login"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupViews()
+        setConstraints()
     }
 
-
+    private func setupViews() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(backgroundView)
+        scrollView.addSubview(loginLabel)
+    }
+    
+    
 }
 
+//MARK: - setConstraints
+
+extension ViewController {
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
+        
+            backgroundView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
+            
+            loginLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 350),
+            loginLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            
+        ])
+    }
+}
