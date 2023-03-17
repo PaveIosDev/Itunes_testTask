3487bd5c3c498c4d9154412ede61168466f81526
Pavelweb22 <pavelweb2016@yandex.ru>
2023-03-17 21:01:04 +0300

    NetworkDataFetch

---
+++ Itunes_testTask/Network/NetworkDataFetch.swift
@@ -0,0 +1,34 @@
+//
+//  NetworkDataFetch.swift
+//  Itunes_testTask
+//
+//  Created by Павел Яковенко on 17.03.2023.
+//
+
+import Foundation
+
+class NetworkDataFetch {
+    
+    static let shared = NetworkDataFetch()
+    
+    private init() {}
+    
+    func fetchAlbum(urlString: String, response: @escaping (AlbumModel?, Error?)-> Void) {
+        
+        NetworkRequest.shared.requestData(urlString: urlString) { result in
+            switch result {
+                
+            case .success(let data):
+                do {
+                    let albums = try JSONDecoder().decode(AlbumModel.self, from: data)
+                    response(albums, nil)
+                } catch let jsonError {
+                        print("Failed to decode JSON", jsonError) 
+                }
+            case .failure(let error):
+                print("Error recived requesting data: \(error.localizedDescription)")
+                response(nil, error)
+            }
+        }
+    }
+}
